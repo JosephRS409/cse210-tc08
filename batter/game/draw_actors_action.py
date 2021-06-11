48a962366eb65b93b99071e03f6b9369dbff6212
AlanWangsgard <alanwangsgard@byui.edu>
2021-06-10 21:15:51 -0600

    I am literally a genius

--- batter/game/draw_actors_action.py
+++ batter/game/draw_actors_action.py
@@ -1,31 +1,17 @@
 from game.action import Action
 
 class DrawActorsAction(Action):
-    """A code template for drawing actors. The responsibility of this class of
-    objects is use an output service to draw all actors on the screen.
-    
-    Stereotype:
-        Controller
-
-    Attributes:
-        _output_service (OutputService): An instance of OutputService.
-    """
 
     def __init__(self, output_service):
-        """The class constructor.
-        
-        Args:
-            output_service (OutputService): An instance of OutputService.
-        """
         self._output_service = output_service
+        
 
     def execute(self, cast):
-        """Executes the action using the given actors.
-
-        Args:
-            cast (dict): The game actors {key: tag, value: list}.
-        """
+        actors = cast["paddle"]
+        actor = cast["brick"]
+        ball = cast["ball"]
+        self._output_service.draw_actors(actors)
+        self._output_service.draw_actors(actor)
+        self._output_service.draw_actors(ball)
+        self._output_service.flush_buffer()    
         self._output_service.clear_screen()
-        for group in cast.values():
-            self._output_service.draw_actors(group)
-        self._output_service.flush_buffer()
